e8e5987df337f4fc989bbb39a91384b57b384aad
fbtwitter <rzaugusdi354@gmail.com>
2022-05-25 10:30:41 +0700

    set up login register server

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+const express = require('express')
+const router = express.Router()
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require('../controllers/userController')
+
+const { protect } = require('../middleware/authMiddleware')
+
+router.post('/', registerUser)
+router.post('/login', loginUser)
+router.post('/me', protect, getMe)
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000
+const connectDB = require('./config/db')
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
